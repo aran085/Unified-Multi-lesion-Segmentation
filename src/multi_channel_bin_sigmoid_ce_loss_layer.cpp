db3b151417cbb2992607698e6b21e65eb2ba84de
aran085 <ARAN-085@proton.me>
2023-09-09 11:43:19 +0000

    Add a comment

--- src/multi_channel_bin_sigmoid_ce_loss_layer.cpp
+++ src/multi_channel_bin_sigmoid_ce_loss_layer.cpp
@@ -4,4 +4,9 @@
 
 #include "caffe/layer.hpp"
 #include "caffe/util/math_functions.hpp"
-#include "caffe/layers/multi_channel_bin_sigmoid_ce_lo
+#include "caffe/layers/multi_channel_bin_sigmoid_ce_loss_layer.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void MultiChannelBinSigmoidCrossEn
