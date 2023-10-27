4c30a465dc3c3f4eb9c8faf105c9678e8467b53a
aran085 <ARAN-085@proton.me>
2023-10-27 18:56:12 +0000

    Added latest 1.9.* versions to test matrix

--- src/multi_channel_bin_sigmoid_ce_loss_layer.cpp
+++ src/multi_channel_bin_sigmoid_ce_loss_layer.cpp
@@ -111,4 +111,5 @@ void MultiChannelBinSigmoidCrossEntropyLossLayer<Dtype>::Forward_cpu(
 		 temp_neg_count[i] = temp_count_neg[i][4];
 		 temp_loss_neg[i] =temp_neg_loss[i][4];
 	 }else if(p_c <= temp_count_neg[i][4] + temp_count_neg[i][3]){
-		 tem
+		 temp_neg_count[i] = temp_count_neg[i][4] + temp_count_neg[i][3];
+		 temp_loss_neg[i] =temp_neg_loss[i][4] + t
