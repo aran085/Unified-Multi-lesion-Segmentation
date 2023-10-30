da2c8e5f3c7a0b51b33fa69403df50c1f7ee55ea
aran085 <ARAN-085@proton.me>
2023-10-30 16:59:21 +0000

    added history

--- src/multi_channel_bin_sigmoid_ce_loss_layer.cpp
+++ src/multi_channel_bin_sigmoid_ce_loss_layer.cpp
@@ -116,4 +116,5 @@ void MultiChannelBinSigmoidCrossEntropyLossLayer<Dtype>::Forward_cpu(
 	 }else if(p_c <= temp_count_neg[i][4] + temp_count_neg[i][3] + temp_count_neg[i][2]){
 		 temp_neg_count[i] = temp_count_neg[i][4] + temp_count_neg[i][3] + temp_count_neg[i][2];
 		 temp_loss_neg[i] =temp_neg_loss[i][4] + temp_neg_loss[i][3] + temp_neg_loss[i][2];
-	 }else if(p_c <= temp_count_neg[i][4] + temp_count_neg[i][3] + t
+	 }else if(p_c <= temp_count_neg[i][4] + temp_count_neg[i][3] + temp_count_neg[i][2] + temp_count_neg[i][1]){
+		 temp_neg_count[i] = temp_count_neg[i][4] + temp_count_neg[i][3] + temp_count_n
