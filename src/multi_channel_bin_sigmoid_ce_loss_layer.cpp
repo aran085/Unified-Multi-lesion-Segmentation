28da6ee49377404837ab5fc02e0876bcc762f11a
aran085 <ARAN-085@proton.me>
2023-12-12 12:19:29 +0000

    Delete __init__.py~

--- src/multi_channel_bin_sigmoid_ce_loss_layer.cpp
+++ src/multi_channel_bin_sigmoid_ce_loss_layer.cpp
@@ -212,4 +212,6 @@ void MultiChannelBinSigmoidCrossEntropyLossLayer<Dtype>::Backward_cpu(
 	 }else if(p_c <= temp_count_neg[i][4] + temp_count_neg[i][3] + temp_count_neg[i][2] + temp_count_neg[i][1]){
 		 temp_neg_count[i] = temp_count_neg[i][4] + temp_count_neg[i][3] + temp_count_neg[i][2] + temp_count_neg[i][1];
 	 }else{
-		 temp_neg_count[i] = temp_count_neg[i][4] + tem
+		 temp_neg_count[i] = temp_count_neg[i][4] + temp_count_neg[i][3] + temp_count_neg[i][2] + temp_count_neg[i][1] + temp_count_neg[i][0];
+	 } 
+ 
