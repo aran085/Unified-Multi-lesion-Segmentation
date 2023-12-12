43a0ba6f070f7f4d763acbab555a137bf73ab91c
aran085 <ARAN-085@proton.me>
2023-12-12 12:08:47 +0000

    dirty commit

--- src/multi_channel_bin_sigmoid_ce_loss_layer.cpp
+++ src/multi_channel_bin_sigmoid_ce_loss_layer.cpp
@@ -210,4 +210,6 @@ void MultiChannelBinSigmoidCrossEntropyLossLayer<Dtype>::Backward_cpu(
 	 }else if(p_c <= temp_count_neg[i][4] + temp_count_neg[i][3] + temp_count_neg[i][2]){
 		 temp_neg_count[i] = temp_count_neg[i][4] + temp_count_neg[i][3] + temp_count_neg[i][2];
 	 }else if(p_c <= temp_count_neg[i][4] + temp_count_neg[i][3] + temp_count_neg[i][2] + temp_count_neg[i][1]){
-		 temp_neg_count[i] = temp_count_neg[i][4] + temp_count_neg[i][3]
+		 temp_neg_count[i] = temp_count_neg[i][4] + temp_count_neg[i][3] + temp_count_neg[i][2] + temp_count_neg[i][1];
+	 }else{
+		 temp_neg_count[i] = temp_count_neg[i][4] + tem
