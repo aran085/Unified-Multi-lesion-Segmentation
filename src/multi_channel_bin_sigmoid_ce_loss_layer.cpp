a1f8e61bd1842558c15c1692f8ac5c495977e571
aran085 <ARAN-085@proton.me>
2023-12-18 22:28:27 +0000

    0.2.31

--- src/multi_channel_bin_sigmoid_ce_loss_layer.cpp
+++ src/multi_channel_bin_sigmoid_ce_loss_layer.cpp
@@ -244,4 +244,7 @@ for(int i = 0; i < num_label_;i++){
 				}
 			}else if(p_c <= temp_count_neg[i][4] + temp_count_neg[i][3]){
 				if(gailv >= 0.6 && gailv <=1.0){
-					bottom_diff[idx] = sigmoid_output_data[idx
+					bottom_diff[idx] = sigmoid_output_data[idx] - 0;
+					bottom_diff[idx] *= count_pos/(count_pos+count_neg);
+				}else{
+					bottom_diff[idx] = 0
